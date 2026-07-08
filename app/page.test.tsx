b7b27d9a115d1a6960bc3c28127b57e06824723f
LuvDPT <honhdok1998@gmail.com>
2026-07-08 21:06:55 +0700

    update

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -7,4 +7,9 @@ import Page from "./page";
 it("App Router: Works with Server Components", () => {
   render(<Page />);
   expect(screen.getByRole("heading")).toHaveTextContent("App Router");
+
+});
+it("Displays the Nice99 text", () => {
+  render(<Page />);
+  expect(screen.getByText("Nice99")).toBeInTheDocument();
 });
